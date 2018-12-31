119be022168c4fd791cb842ab8524f518b1326d4
Milo MacPhail <milomacphail@gmail.com>
2018-12-31 16:26:42 +0000

    Added Experience Form

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import { createStore, applyMiddleware } from 'redux';
 
@@ -57,6 +58,7 @@ class App extends Component {
         <Route exact path="/dashboard" component={ Dashboard } />
         <Route exact path="/create-profile" component={ CreateProfile } />
         <Route exact path="/edit-profile" component={ EditProfile } />
+        <Route exact path="/add-experience" component={ AddExperience } />
       </div>
       <Footer />
       </div>
