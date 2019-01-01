e877085d895a6f6a89d296e78c5c814477b4c315
Milo MacPhail <milomacphail@gmail.com>
2019-01-01 23:25:10 +0000

    Created Post Feed

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import {
     ADD_POST,
     GET_ERRORS,
+    GET_POSTS,
+    POST_LOADING
 } from './types';
 
 //Add Post
@@ -22,3 +24,31 @@ export const addPost = postData => dispatch => {
         })
         );
 };
+
+//Get Post
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading());
+    axios
+        .get('/api/posts')
+        .then(res =>
+        dispatch({
+            type: GET_POSTS,
+            payload: res.data
+        })
+        )
+        .catch(err => 
+        dispatch({
+            type: GET_POSTS,
+            payload: null
+        })
+        );
+};
+
+//Set loading state 
+export const setPostLoading = () => {
+    return {
+        type: POST_LOADING
+    }
+    
+    
+}
